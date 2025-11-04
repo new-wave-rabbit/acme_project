517b20633220c134a0c5e9cf0e6e81ba15edc89f
dotsenkois <dotsenkois@yandex.ru>
2025-11-04 18:48:22 +0700

    Добавлено ограничение длмны поля

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 
 class BirthdayForm(forms.Form):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
+    first_name = forms.CharField(max_length=20)
+    last_name = forms.CharField(required=False)
     birthday = forms.DateField()
