3d9787cbc5dca18a30feb4100ce0bbcebd3b14ad
dotsenkois <dotsenkois@yandex.ru>
2025-11-05 23:19:35 +0700

    Добавен сахар

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -6,19 +6,8 @@ from .forms import BirthdayForm
 
 
 def birthday(request):
-    # Если есть параметры GET-запроса...
-    if request.GET:
-        # ...передаём параметры запроса в конструктор класса формы.
-        form = BirthdayForm(request.GET)
-        # Если данные валидны...
-        if form.is_valid():
-            # ...то считаем, сколько дней осталось до дня рождения.
-            # Пока функции для подсчёта дней нет — поставим pass:
-            pass
-    # Если нет параметров GET-запроса.
-    else:
-        # То просто создаём пустую форму.
-        form = BirthdayForm()
-    # Передаём форму в словарь контекста:
+    form = BirthdayForm(request.GET or None)
+    if form.is_valid():
+        pass
     context = {'form': form}
     return render(request, 'birthday/birthday.html', context)
