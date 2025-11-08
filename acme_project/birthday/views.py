072fcf192a72001eff2072357c2dc18cb5eb2249
dotsenkois <dotsenkois@yandex.ru>
2025-11-08 15:13:10 +0700

    добавлено сохранение данных из формы

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -6,11 +6,12 @@ from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 
 def birthday(request):
-    form = BirthdayForm(request.GET or None)
+    form = BirthdayForm(request.POST  or None)
     # Создаём словарь контекста сразу после инициализации формы.
     context = {'form': form}
     # Если форма валидна...
     if form.is_valid():
+        form.save()
         # ...вызовем функцию подсчёта дней:
         birthday_countdown = calculate_birthday_countdown(
             # ...и передаём в неё дату из словаря cleaned_data.
