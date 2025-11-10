d875b742727ccc20a613a2d54f042aa8c9ee46a0
dotsenkois <dotsenkois@yandex.ru>
2025-11-10 22:32:12 +0700

    Настроены валидаторы

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -34,6 +34,7 @@ class BirthdayForm(forms.ModelForm):
         return first_name.split()[0]
 
     def clean(self):
+        super().clean()
         # Получаем имя и фамилию из очищенных полей формы.
         first_name = self.cleaned_data['first_name']
         last_name = self.cleaned_data['last_name']
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -8,3 +8,11 @@ class Birthday(models.Model):
         'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
     )
     birthday = models.DateField('Дата рождения', validators=(real_age,))
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
