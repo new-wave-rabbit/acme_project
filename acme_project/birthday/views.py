2709a1bebb013965f75ea85876a65eba0fd1595c
dotsenkois <dotsenkois@yandex.ru>
2025-11-15 14:28:38 +0700

    Добавлено переопределение котекста

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -72,5 +72,16 @@ class BirthdayListView(ListView):
 
 class BirthdayDetailView(DetailView):
     model = Birthday
-    template_name_suffix = '_detail' 
+    template_name_suffix = '_detail'
+
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста:
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь новый ключ:
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            # Дату рождения берём из объекта в словаре context:
+            self.object.birthday
+        )
+        # Возвращаем словарь контекста.
+        return context
 
