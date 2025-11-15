96f080608379f5b0a2ac6ed096e42b67cfdf77c9
dotsenkois <dotsenkois@yandex.ru>
2025-11-15 14:35:58 +0700

    Убрал Mixin

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -8,29 +8,22 @@ from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
 
-
 class BirthdayListView(ListView):
     model = Birthday
     ordering = 'id'
     paginate_by = 10
 
+class BirthdayCreateView(CreateView):
+    model = Birthday
+    form_class = BirthdayForm
 
-class BirthdayMixin:
+class BirthdayUpdateView(UpdateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+class BirthdayDeleteView(DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
-
-
-class BirthdayCreateView(BirthdayMixin, CreateView):
-    form_class = BirthdayForm
-
-
-class BirthdayUpdateView(BirthdayMixin, UpdateView):
-    form_class = BirthdayForm
-
-
-class BirthdayDeleteView(BirthdayMixin, DeleteView):
-    pass
-
 
 class BirthdayDetailView(DetailView):
     model = Birthday
