111acf87b50981a655922f7d239150ab544f352b
dotsenkois <dotsenkois@yandex.ru>
2025-11-15 14:34:57 +0700

    Добавлена функция получения абсолютного адреса

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,6 +1,9 @@
 # birthday/models.py
 from django.db import models
 from .validators import real_age
+# Импортируем функцию reverse() для получения ссылки на объект.
+from django.urls import reverse
+
 
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
@@ -17,3 +20,7 @@ class Birthday(models.Model):
                 name='Unique person constraint',
             ),
         )
+
+    def get_absolute_url(self):
+        # С помощью функции reverse() возвращаем URL объекта.
+        return reverse('birthday:detail', kwargs={'pk': self.pk}) 
