d71b33096fe14fb539eea9bf67fc9de72dd9eb62
dotsenkois <dotsenkois@yandex.ru>
2025-11-15 12:51:07 +0700

    Удаление объектов через CBV

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import CreateView, ListView, UpdateView
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
@@ -78,4 +78,6 @@ class BirthdayListView(ListView):
     # ...и даже настройки пагинации:
     paginate_by = 10
 
-
+class BirthdayDeleteView(DeleteView):
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
