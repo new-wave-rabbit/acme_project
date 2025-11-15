d46eb0cf67e9f44ce9857b754eabec5a33554db0
dotsenkois <dotsenkois@yandex.ru>
2025-11-15 16:06:35 +0700

    главная страница переведена на CBV

--- acme_project/pages/views.py
+++ acme_project/pages/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+# pages/views.py
+
+# Импортируем класс TemplateView, чтобы унаследоваться от него.
+from django.views.generic import TemplateView
 
 
-def homepage(request):
-    return render(request, 'pages/index.html')
+class HomePage(TemplateView):
+    # В атрибуте template_name обязательно указывается имя шаблона,
+    # на основе которого будет создана возвращаемая страница.
+    template_name = 'pages/index.html'
