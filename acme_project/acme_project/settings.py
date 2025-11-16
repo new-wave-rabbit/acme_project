416d91a8e6fcfa55e5c39de3ccb7741f59ec6a03
dotsenkois <dotsenkois@yandex.ru>
2025-11-16 18:48:18 +0700

    ДОбавлен email-бекенд

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -93,3 +93,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'users.MyUser' 
+
+# Подключаем бэкенд filebased.EmailBackend:
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# Указываем директорию, в которую будут сохраняться файлы писем:
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails' 
