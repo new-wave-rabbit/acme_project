d7be9f95175c178fc9ccb57b2375fb07daed05be
dotsenkois <dotsenkois@yandex.ru>
2025-11-16 17:50:43 +0700

    Расширена модель пользователя

--- acme_project/users/models.py
+++ acme_project/users/models.py
@@ -1,3 +1,7 @@
+# users/models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class MyUser(AbstractUser):
+    bio = models.TextField('Биография', blank=True)
