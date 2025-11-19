2c1bf4c1d38494818447c4243c4ebce532d58897
idotsenko <idotsenko@usergate.com>
2025-11-19 10:49:17 +0300

    Добавлен django debug_toolbar

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -7,20 +7,20 @@ from django.contrib import admin
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import include, path, reverse_lazy
 from django.views.generic.edit import CreateView
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
+    path("__debug__/", include("debug_toolbar.urls")),
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
     path('auth/', include('django.contrib.auth.urls')),
     path(
-        'auth/registration/', 
+        'auth/registration/',
         CreateView.as_view(
             template_name='registration/registration_form.html',
             form_class=UserCreationForm,
             success_url=reverse_lazy('pages:homepage'),
         ),
         name='registration',
-    )
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + debug_toolbar_urls()
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
