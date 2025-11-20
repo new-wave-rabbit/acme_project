c70f52341ba6de68bd1a96cf81adf05e7b464c55
idotsenko <idotsenko@usergate.com>
2025-11-20 15:45:23 +0300

    Уменьшено количество запросов

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -23,6 +23,12 @@ class OnlyAuthorMixin(UserPassesTestMixin):
 
 class BirthdayListView(ListView):
     model = Birthday
+    # По умолчанию этот класс
+    # выполняет запрос queryset = Birthday.objects.all(),
+    # но мы его переопределим:
+    queryset = Birthday.objects.prefetch_related(
+        'tags'
+    ).select_related('author')
     ordering = 'id'
     paginate_by = 10
 
