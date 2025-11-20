4bc072fd1c1074be401a01863d10cd8687d8f8aa
idotsenko <idotsenko@usergate.com>
2025-11-20 10:35:06 +0300

    Добавлен миксин LoginRequiredMixin

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -7,25 +7,29 @@ from django.urls import reverse_lazy
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class BirthdayListView(ListView):
     model = Birthday
     ordering = 'id'
     paginate_by = 10
 
-class BirthdayCreateView(CreateView):
+class BirthdayCreateView(LoginRequiredMixin, CreateView):
     model = Birthday
     form_class = BirthdayForm
 
-class BirthdayUpdateView(UpdateView):
+class BirthdayUpdateView(LoginRequiredMixin, UpdateView):
     model = Birthday
     form_class = BirthdayForm
 
-class BirthdayDeleteView(DeleteView):
+class BirthdayDeleteView(LoginRequiredMixin, DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
 
-class BirthdayDetailView(DetailView):
+class BirthdayDetailView(LoginRequiredMixin, DetailView):
     model = Birthday
 
     def get_context_data(self, **kwargs):
